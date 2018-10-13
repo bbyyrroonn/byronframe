37b0d7425fa691c096fe35e86076ef81b0914823
bbyyrroonn <bbyyrroonn@163.com>
2018-10-13 17:12:00 +0800

    commit all files
    commit all files

---
+++ front/src/module/index/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <login></login>
+  </div>
+</template>
+
+<script>
+  import 'common/css/bootstrap.css';
+  import 'common/css/font-awesome.css';
+  import 'common/css/beyond.css';
+
+  import Login from 'components/login/login'
+
+  export default {
+    name: 'app',
+    components: {
+      Login
+    }
+  }
+</script>
+
+<style>
+
+</style>
